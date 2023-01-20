56b5fc0ddcb85d35b99ebffca62e96b44cbd9469
PavelMikhailov <n9032692530@gmail.com>
2023-01-20 19:28:47 +0300

    Домашнее задание

---
+++ src/Family.java
@@ -0,0 +1,84 @@
+public class Family {
+
+    static int N = 20; // Максимальное количество детей
+
+
+    static class Node {
+        char val;
+
+        Node[] child = new Node[N];
+
+        Node(char Pip) {
+
+            val = Pip;
+
+            for (int i = 0; i < N; i++){
+                child[i] = null;
+            }
+        }
+    }
+
+
+    static void printChild(Node root, char Pip, int chain){
+        if (root.val == Pip) {
+            if (root.child[chain - 1] == null){
+                System.out.print("Ошибка: Данные не найдены\n");
+            }
+            else {
+                System.out.print(root.child[chain - 1].val + "\n");
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (root.child[i] != null) {
+                printChild(root.child[i], Pip, chain);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Node root = new Node('A');
+
+        root.child[0] = new Node('B');
+
+        root.child[1] = new Node('C');
+
+        root.child[2] = new Node('D');
+
+        root.child[3] = new Node('E');
+
+        root.child[0].child[0] = new Node('F');
+
+        root.child[0].child[1] = new Node('G');
+
+        root.child[2].child[0] = new Node('H');
+
+        root.child[0].child[0].child[0] = new Node('I');
+
+        root.child[0].child[0].child[1] = new Node('J');
+
+        root.child[0].child[0].child[2] = new Node('K');
+
+        root.child[2].child[0].child[0] = new Node('L');
+
+        root.child[2].child[0].child[1] = new Node('M');
+
+
+        char Pip = 'B';
+
+        System.out.print("Второй ребёнок B: ");
+
+        printChild(root, Pip, 2);
+
+
+        Pip = 'A';
+
+        System.out.print("Седьмой ребёнок А: ");
+
+        printChild(root, Pip, 7);
+
+    }
+
+}
+
